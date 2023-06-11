b0a68a21f9d322db2593399c10c1f00098311cbf
Даниил <45365913+chashkovdaniil@users.noreply.github.com>
2023-06-11 22:20:20 +0500

    remove some_package

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
-// import 'package:equatable/equatable.dart';
-import 'package:some_package/some_package.dart';
+// import 'package:some_package/some_package.dart';
+import 'package:chopper/chopper.dart';
 import 'package:yet_some_package/yet_some_package.dart';
 
 void main() {
   // print('Hello, ${Class().toString()}');
-  print('Hello, ${SomeClass().toString()}');
+  ChopperClient;
   print('Hello, ${YetSomeClass().toString()}');
 }
-
-// class Class extends Equatable {
-//   final name = 'name';
-//   @override
-//   List<Object> get props => [name];
-// }
